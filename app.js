7b82dbd000a8da421e7b1e164fe8eed38e6ea230
enkod-id <natanaeltamto@gmail.com>
2023-09-06 15:50:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/', async (req, res) => {
 const port = process.env.PORT || 3000;
 
 
-const port = 3000;
+
 
 app.listen(port, () => {
     console.log(`Running on port http://locahost:${port}`)
